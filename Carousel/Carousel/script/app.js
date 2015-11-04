0efff3ed62bea6e650a6f55653f445f5b4f69912
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-11-05 01:36:46 +0200

    v0.2
    TODO: clicks on the dots; zoom out; 3 patterns at the same time

--- Carousel/Carousel/script/app.js
+++ Carousel/Carousel/script/app.js
@@ -3,10 +3,54 @@
     var arrCars = ["img\\cars\\cars1.jpg", "img\\cars\\cars2.jpg", "img\\cars\\cars3.jpg", "img\\cars\\cars4.jpg", "img\\cars\\cars5.jpg"];    
     var arrGames = ["img\\games\\game1.jpg", "img\\games\\game2.jpg", "img\\games\\game3.jpg", "img\\games\\game4.jpg", "img\\games\\game5.jpg" ];
     var smallButtonPointer = ['#goTo2'];
-    voidLoop();
     
+    arrFurniture = shuffle(arrFurniture);
+    arrCars = shuffle(arrCars);
+    arrGames = shuffle(arrGames);
+    var interval;
 
-    function voidLoop() { 
+    $('#buttFurniture').click(function () {
+        $(this).css("background-color", "#656565");        
+        $('#buttCars').css("background-color", "#F1F1F1");
+        $('#buttGames').css("background-color", "#F1F1F1");
+        $(this).css("color", "white");
+        $('#buttCars').css("color", "black");
+        $('#buttGames').css("color", "black");
+
+        clearInterval(interval);
+        interval = voidLoop(arrFurniture);
+    });
+    
+    $('#buttCars').click(function () {
+        $(this).css("background-color", "#656565");
+        $('#buttFurniture').css("background-color", "#F1F1F1");
+        $('#buttGames').css("background-color", "#F1F1F1");
+        $(this).css("color", "white");
+        $('#buttFurniture').css("color", "black");
+        $('#buttGames').css("color", "black");
+
+        clearInterval(interval);
+        interval = voidLoop(arrCars);
+    });
+    
+    $('#buttGames').click(function () {        
+        $(this).css("background-color", "#656565");
+        $('#buttCars').css("background-color", "#F1F1F1");
+        $('#buttFurniture').css("background-color", "#F1F1F1");
+        $(this).css("color", "white");
+        $('#buttCars').css("color", "black");
+        $('#buttFurniture').css("color", "black");
+
+        clearInterval(interval);
+        interval = voidLoop(arrGames);
+    });
+
+    function shuffle(o) {
+        for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x)        ;
+        return o;
+    }
+
+    function voidLoop(arr) { 
         var left = 0;
         var middle = 1;
         var right = 2;
@@ -32,10 +76,34 @@
                 middle++;
                 right++;
             }
-            $('#leftSide').attr('src', arrFurniture[left]);
-            $('#middle').attr('src', arrFurniture[middle]);
-            $('#rightSide').attr('src', arrFurniture[right]);
-            $('smallButtonPointer[0]').attr('id', 'currentMiddleImage');
-        }, 5000);
+            $('#leftSide').attr('src', arr[left]);
+            $('#middle').attr('src', arr[middle]);
+            $('#rightSide').attr('src', arr[right]);
+            changeColor(middle);           
+        }, 500);
+    }
+    function changeColor(index) {
+        switch (index) {
+            case 0:
+                $('#goTo0').attr('class', 'currentMiddleImage');
+                $('#goTo4').attr('class', 'bottomButt');       
+                break;
+            case 1:
+                $('#goTo1').attr('class', 'currentMiddleImage');
+                $('#goTo0').attr('class', 'bottomButt');
+                break;
+            case 2:
+                $('#goTo2').attr('class', 'currentMiddleImage');
+                $('#goTo1').attr('class', 'bottomButt');
+                break;
+            case 3:
+                $('#goTo3').attr('class', 'currentMiddleImage');
+                $('#goTo2').attr('class', 'bottomButt');
+                break;
+            case 4:
+                $('#goTo4').attr('class', 'currentMiddleImage');
+                $('#goTo3').attr('class', 'bottomButt');
+                break;
+        }
     }
 });
